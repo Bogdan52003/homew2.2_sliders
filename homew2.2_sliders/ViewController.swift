51a8a37f85e95189f5c8c40bf3bb37f20f62bccf
Bogdan52003 <m2112619@edu.misis.ru>
2023-11-26 11:18:39 +0300

    Initial Commit

---
+++ homew2.2_sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homew2.2_sliders
+//
+//  Created by Bogdan Chupakhin on 26.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
